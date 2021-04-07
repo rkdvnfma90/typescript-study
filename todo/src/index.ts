cfd1dbcf00f2c596cfd121031a8be6cb0263ca3f
DESKTOP-BGEFQLI\Administrator <rkdvnfma90@hanmail.net>
2021-04-07 20:14:59 +0900

    feat 타입 설정

--- todo/src/index.ts
+++ todo/src/index.ts
@@ -1,52 +1,68 @@
-let todoItems
+// type Todo = {
+//   id: number;
+//   title: string;
+//   done: boolean;
+// };
+
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+let todoItems: Todo[] = [];
 
 // api
-function fetchTodoItems() {
+function fetchTodoItems(): Todo[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
     { id: 3, title: '스크립트', done: false },
-  ]
-  return todos
+  ];
+  return todos;
 }
 
 // crud methods
-function fetchTodos() {
-  const todos = fetchTodoItems()
-  return todos
+function fetchTodos(): Todo[] {
+  const todos = fetchTodoItems();
+  return todos;
 }
 
-function addTodo(todo) {
-  todoItems.push(todo)
+function addTodo(todo: Todo): void {
+  todoItems.push(todo);
 }
 
-function deleteTodo(index) {
-  todoItems.splice(index, 1)
+function deleteTodo(index: number): void {
+  todoItems.splice(index, 1);
 }
 
-function completeTodo(index, todo) {
-  todo.done = true
-  todoItems.splice(index, 1, todo)
+function completeTodo(index: number, todo: Todo): void {
+  todo.done = true;
+  todoItems.splice(index, 1, todo);
 }
 
 // business logic
-function logFirstTodo() {
-  return todoItems[0]
+function logFirstTodo(): object {
+  return todoItems[0];
 }
 
-function showCompleted() {
-  return todoItems.filter(item => item.done)
+function showCompleted(): object[] {
+  return todoItems.filter(item => item.done);
 }
 
 // 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수
-function addTwoTodoItems() {
-  // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야함
+function addTwoTodoItems(): void {
+  const item1 = { id: 4, title: '제주도 가기', done: false };
+  const item2 = { id: 5, title: '여수 가기', done: false };
+
+  addTodo(item1);
+  addTodo(item2);
 }
 
-function log() {
-  console.log(todoItems)
+function log(): void {
+  console.log(todoItems);
 }
 
-todoItems = fetchTodoItems()
-addTwoTodoItems()
-log()
+todoItems = fetchTodoItems();
+addTwoTodoItems();
+log();
