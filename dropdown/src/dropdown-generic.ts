4df1100fa0ffee60ee402ab4c56c692a9d7bb41b
DESKTOP-BGEFQLI\Administrator <rkdvnfma90@hanmail.net>
2021-04-08 15:04:44 +0900

    feat 제네릭 예제

---
+++ dropdown/src/dropdown-generic.ts
@@ -0,0 +1,35 @@
+interface DropdownItem<T> {
+  value: T;
+  selected: boolean;
+}
+
+const emails: DropdownItem<string>[] = [
+  { value: 'naver.com', selected: true },
+  { value: 'gmail.com', selected: false },
+  { value: 'hanmail.net', selected: false },
+];
+
+const numberOfProducts: DropdownItem<number>[] = [
+  { value: 1, selected: true },
+  { value: 2, selected: false },
+  { value: 3, selected: false },
+];
+
+function createDropdownItem(item: DropdownItem<string> | DropdownItem<number>) {
+  const option = document.createElement('option');
+  option.value = item.value.toString();
+  option.innerText = item.value.toString();
+  option.selected = item.selected;
+  return option;
+}
+
+emails.forEach(email => {
+  const item = createDropdownItem(email);
+  const selectTag = document.querySelector('#email-dropdown');
+  selectTag.appendChild(item);
+});
+
+numberOfProducts.forEach(product => {
+  const item = createDropdownItem(product)
+  // ...
+})
